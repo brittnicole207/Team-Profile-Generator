bc81147635b23f71b52b3cae77271ace553f43cd
BRITTANY BLALOCK <brittany.blalock2@gmail.com>
2022-10-28 08:19:29 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,212 @@
+//index.js runs the application 
+
+const fs = require("fs"); 
+const inquirer = require("inquirer");
+const renderPage = require ("./src/page-template.js");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require ("./lib/Intern");
+
+const teamArray =[];
+
+//Callback function for a manager to enter their manager details
+const promptManager = () => {
+    return inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?",
+        validate: nameProvided => {
+            if (nameProvided) {
+                return true; 
+            } else {
+            console.log("Please enter the manager's name.")
+            return false;
+        }
+    }
+},
+{
+        type: "input",
+        name: "email",
+        message: "Please enter the manager's email: ",
+        validate: emailProvided => {
+            if (emailProvided) {
+                return true; 
+            } else {
+            console.log("Please enter the manager's email.")
+            return false;
+            }
+        } 
+    },
+    {
+    type: "input",
+    name: "id",
+    message: "Please provide the the manager's ID: ",
+    validate: id => {
+        if (isNaN(id)) {
+        console.log("Please enter the manager's ID.");
+        return false;
+    } else {
+    return true; 
+    }
+    }
+    },
+{
+    type: "input",
+    name: "officeNumber",
+    message: "Please enter the office number: ",
+    validate: userInput => {
+       if (userInput) {
+        return true;
+    } else {
+            console.log("Please enter the manager's office number."); 
+            return false; 
+            }
+        }
+    }
+])
+.then(managerInfo => {
+    const {name, email, id, officeNumber} = managerInfo;
+    const manager = new Manager (name, email, id, officeNumber);
+
+        teamArray.push(manager);
+        console.log(manager);
+    })
+};
+
+// The manager will get a view on the command line for the option to add an employee. 
+const promptEmployee = () => {
+    console.log(`
+    ==================
+    Add employees
+    ==================
+`);
+
+//There will be a list for the manager to select if they want to add an engineer or an intern. 
+return inquirer.prompt ([
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Engineer", "Intern"]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?", 
+        validate: nameEntered => {
+        if (nameEntered) {
+            return true; 
+        } else {
+        console.log("Please enter the employee's name.");
+        return false;
+        }
+    }
+},
+{
+        type: "input",
+        name: "email", 
+        message: "Please enter the employee's email: ",
+        validate: emailEntered => {
+        if  (emailEntered) {
+            return true; 
+        } else {
+            console.log("Please enter the employee's email.");
+        return false;
+        }
+    }
+},
+{
+        type: "input",
+        name: "id", 
+        message: "Please provide the employee's ID: ",
+        validate: id => {
+            if (isNaN(id)) {
+            console.log("Please enter the employee's ID."); 
+            return false;
+        } else {
+            return true; 
+        }
+    }
+},
+{
+    type: "input",
+    name: "github",
+    message: "Please enter the engineer's GitHub username: ", 
+    when: (input) => input.role === "Engineer",
+    validate: githubName => {
+    if (githubName) {
+        return true;
+        } else {
+        console.log("Please enter the engineer's GitHub username.");
+        return false;
+        }
+    }
+},
+{   type: "input",
+    name: "school",
+    message: "Please provide the intern's school: ",
+    when: (input) => input.role === "Intern",
+    validate: schoolEntered => {
+        if (schoolEntered) {
+            return true;
+        } else { 
+            console.log("Please enter the intern's school.")
+        }
+    }
+},
+{
+    type: "confirm",
+    name: "confirmAdd",
+    message: "Would you like to add another employee?",
+    default: false 
+    }
+])
+//When the manager says that they do not want to add another employee, theyn it will populate all the data that the Command Line requested from the manager. 
+    .then(employeeData => {
+        let {name, email, id, role, github, school, confirmAdd} = employeeData;
+        let employee = employeeData; 
+
+        if (role === "Engineer") {
+            employee = new Engineer(name, email, id, github); 
+
+            console.log(employee);
+        }
+        else if (role === "Intern") {
+            employee = new Intern(name, email, id, school);
+
+            console.log(employee);
+        }
+        teamArray.push(employee);
+
+        if (confirmAdd) {
+            return promptEmployee(teamArray);
+        } else {
+            console.log(teamArray);
+            return teamArray;
+        }
+    })
+};
+
+//This will take the data and put it into the JS template which will populate into the HTML file. 
+function writeToFile(data) {
+    fs.writeFile("./dist/index.html", data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success!');
+    });
+}
+
+promptManager()
+    .then(promptEmployee) 
+    .then(teamArray => {
+        return renderPage(teamArray);
+    })
+    .then(pageHTML => {
+        return writeToFile(pageHTML);
+    })
+.catch(err => {
+    console.log(err)
+})
---
+++ src/page-template.js
@@ -0,0 +1,128 @@
+//Display Manager Card 
+//The page.js will begin to read the JS as HTML with the back-tick. Using bootstrap to style the page.
+const displayManager = manager => {
+    return ` 
+        <div class="shadow col-w flex column m-2 p-0">
+            <div class ="bg-info px-2 pb-1">
+                <h2>${manager.name}</h2>
+                <h3>Manager</h3>
+            </div>
+
+            <div> class="flex-wrap flex-column p-0">
+                <ul class="list-unstyled m-0 p-2">
+                    <li class="bg-white border border-dark px-1">ID: ${manager.id}</li> 
+                    <li class="bg-white border border-dark px-1">Email: ${manager.email}</li>
+                    <li class="bg-white border border-dark px-1">Office Number: ${manager.officeNumber}</li>
+                </ul>
+            </div>
+        </div> 
+    `
+}
+
+//Gathers engineer name, id, email, and github from user input about the engineer and puts it into the engineer card and then displays engineer card.
+//Styling with Bootstrap 
+const displayEngineer = engineer => {
+    return `
+        <div class="shadow col-2 flex-column m-2 p-0">
+            <div class="bg-info px-2 pb-1"> 
+                <h2>${engineer.name}</h2>
+                <h3>Engineer</h3> 
+            </div>
+
+            <div class="flex-wrap flex-column p-0"> 
+                <ul class="list-unstyled m-0 p-2">
+                    <li class="bg-white border border-dark px-1">ID: ${engineer.id}</li>
+                    <li class="bg-white border border-dark px-1">Email: ${engineer.email}</li>
+                    <li class="bg-white border border-dark px-1">Github username: ${engineer.github}</li>
+                </ul>
+            </div>
+        </div>
+    `
+}
+
+//Display Intern Card
+const displayIntern = intern => {
+    return `
+        <div class="shadow col-2 flex-column m-2 p-0">
+            <div class="bg-info px-2 pb-1">
+                <h2>${intern.name}</h2>
+                <h3>Intern</h3>
+            </div> 
+
+            <div class="flex-wrap flex-column p-0"> 
+                <ul class="list-unstyled m-0 p-2">
+                    <li class="bg-white border border-dark px-1">ID: ${intern.id}</li>
+                    <li class="bg-white border border-dark px-1">Email: ${intern.email}</li>
+                    <li class="bg-white border border-dark px-1">School: ${intern.school}</li>
+                </ul>
+            </div>
+        </div>
+    `
+}
+
+//Page Renders 
+renderPage = data => {
+    teamArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+        console.log(role); 
+
+        if (role === "Manager") {
+            const createManager = displayManager(employee);
+
+            teamArray.push(createManager);
+        }
+
+        if (role === "Engineer") {
+            const createManager = displayEngineer(employee);
+
+            teamArray.push(createManager);
+        }
+
+        if (role === "Intern") {
+            const createIntern = displayIntern(employee); 
+
+        teamArray.push(createIntern);
+    }
+}
+
+    const teamDisplay = teamArray.join("");
+
+    const renderTeam = createTeamHTML(teamDisplay);
+    return renderTeam;
+}
+
+//create team member profile box
+const createTeamHTML = teamDisplay => {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profiles</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+      </head>
+
+      <body>
+        <header>
+          <div class="text-center p-3 mb-2 bg-danger">
+            <h1>The Team</h1>
+          </div>
+        </header>
+
+        <section class="d-flex flex-row flex-wrap justify-content-center">
+          ${teamDisplay}
+        </section>
+        
+      </body>
+      </html>
+    `
+  }
+  
+  module.exports = renderPage;
